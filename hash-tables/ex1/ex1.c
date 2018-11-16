f65390df6361c0ae8b22c8d80780e83ee35bdbae
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-11-16 10:45:57 -0600

    wrote ex1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -8,7 +8,24 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
   HashTable *ht = create_hash_table(16);
 
   // YOUR CODE HERE 
-
+  //weights need to add up to the limit
+  //length is the amount of items needed to create the limit sum.
+  //loop through twice to find the indices of the items 
+  //a set of if statements the determine if the weights add up to the limit.
+  //return two indices
+  LinkedPair *index_1;
+  LinkedPair *index_2;
+  for(int i = 0; i < ht->storage; i++){
+    for(int j = 0; j < ht->storage; j++){
+      int index_1 = ht->storage[i];
+      int index_2 = ht->storage[j];
+      if(index_1 + index_2 > limit || index_1 + index_2 < limit){
+        break;
+      } else if( index_1 + index_2 == limit){
+        return index_1, index_2; 
+      }
+    }
+  }
   return NULL;
 }
 
