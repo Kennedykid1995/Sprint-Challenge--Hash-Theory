4260b3d50247d7dcfc0cc30d344318e48de254ce
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-11-16 12:03:41 -0600

    getting segmentation fault ex1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -3,28 +3,44 @@
 #include "hashtable.h"
 #include "ex1.h"
 
+int main(void)
+{
+  int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
+
+  Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+
+  if (answer_4->index_1 != 6)
+    printf("Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}");
+  if (answer_4->index_2 != 2)
+    printf("Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}");
+
+  return 0;
+}
+
+
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
-
+  Answer *answer = NULL;
   // YOUR CODE HERE 
   //weights need to add up to the limit
   //length is the amount of items needed to create the limit sum.
-  //loop through twice to find the indices of the items 
-  //a set of if statements the determine if the weights add up to the limit.
+  //loop through to find the indices of the items 
+  //a  if statement that determines if the weights add up to the limit.
   //return the amount of indices needed to create limit.
-  LinkedPair *index_1;
-  LinkedPair *index_2;
-  for(int i = 0; i < ht->storage; i++){
-    for(int j = 0; j < ht->storage; j++){
-      int index_1 = ht->storage[i];
-      int index_2 = ht->storage[j];
-      if(index_1 + index_2 > limit || index_1 + index_2 < limit){
-        break;
-      } else if( index_1 + index_2 == limit){
-        return index_1, index_2; 
-      }
+  
+  for(int i = 0; i < length; i++){
+    hash_table_insert(ht, weights[i], i);
+  }
+  for(int i = 0; i < length; i ++){
+    int retrieve = hash_table_retrieve(ht, (limit - weights[i]));
+    if (retrieve != -1){
+      if(i > retrieve){
+        answer->index_1=i;
+        answer->index_2=retrieve;
+      } 
     }
+    return answer;
   }
   return NULL;
 }
